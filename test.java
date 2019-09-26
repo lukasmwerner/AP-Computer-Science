35e0cb299c6ddf28cac3ebc25973a66521977e71
Lukas Werner <program4cool@gmail.com>
2019-09-26 13:33:58 -0700

    add test.java

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+        int num = 22;
+        if (num > 0)
+            if (num % 5 == 0) 
+                System.out.println(num);
+            else 
+                System.out.println(num + " negative");
+    }
+}
