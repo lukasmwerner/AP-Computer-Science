eb23edccf486e948a47f671eb67f7f63dcb1fbb0
Lukas Werner <lwerner@lshigh.org>
2019-10-07 09:46:34 -0700

    Fix things

--- UnitOne/finalProj/Adventure.java
+++ UnitOne/finalProj/Adventure.java
@@ -1,10 +1,3 @@
-/* 
-    Lukas Werner 
-    (c) 2019 
-    AP Computer Science
-    Created for Unit One Project
-*/
-
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -15,34 +8,37 @@ public class Adventure {
         int[][] playField = new int[20][20]; // create the playfield 2D array to contain our enemies and items
         boolean gameRunning = true; // create our main quitting boolean for our sentinal loop
         /*
-         * Object IDs: 1 = Player | 2 = Enemy | 3 = Sword | 4 = Treasure | 5 = Hole | 6 = Crown
+         * Object IDs: 1 = Player | 2 = Enemy | 3 = Sword | 4 = Treasure | 5 = Hole | 6
+         * = Crown
          */
 
         playField[10][10] = 1; // set player in center of field
 
-        playField[0][0] = 2;
+        int[] randomPositon = { (int) (Math.random() * 20), (int) (Math.random() * 20) };
+
+        playField[randomPositon[0]][randomPositon[1]] = 2; // Enemy
 
         while (gameRunning) {
 
             render(playField);
             String direction = kb.nextLine();
-            if (direction.toLowerCase().equals("n")) {
+            collisonCheck(playField);
+            if (direction.toUpperCase().equals("N")) {
                 int x = findTypePosition(playField, 1).get(0);
                 int y = findTypePosition(playField, 1).get(1);
-                moveEntity(playField, 1, x-1, y);
-            } else if (direction.toLowerCase().equals("s")){
+                moveEntity(playField, 1, x - 1, y);
+            } else if (direction.toUpperCase().equals("S")) {
                 int x = findTypePosition(playField, 1).get(0);
                 int y = findTypePosition(playField, 1).get(1);
-                moveEntity(playField, 1, x+1, y);
-            }
-            else if (direction.toLowerCase().equals("e")) {
+                moveEntity(playField, 1, x + 1, y);
+            } else if (direction.toUpperCase().equals("E")) {
                 int x = findTypePosition(playField, 1).get(0);
                 int y = findTypePosition(playField, 1).get(1);
-                moveEntity(playField, 1, x, y+1);
-            } else if (direction.toLowerCase().equals("w")) {
+                moveEntity(playField, 1, x, y + 1);
+            } else if (direction.toUpperCase().equals("W")) {
                 int x = findTypePosition(playField, 1).get(0);
                 int y = findTypePosition(playField, 1).get(1);
-                moveEntity(playField, 1, x, y-1);
+                moveEntity(playField, 1, x, y - 1);
             }
 
         }
@@ -50,19 +46,22 @@ public class Adventure {
     }
 
     // Seccondary 'libraries' that help with sanity and shorter function
-    public static void print(String sentString) {
-        System.out.print(sentString);
+    public static void print(Object sent) {
+        System.out.print(sent);
     }
 
-    public static void printf(String sentString) {
+    public static void printf(Object sentString) {
         System.out.println(sentString);
     }
 
     public static void render(int[][] playField) {
         for (int[] row : playField) {
-            for (int item: row) {
-                System.out.print(item);
-                
+            for (int item : row) {
+                if (item == 0) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print(item);
+                }
             }
             System.out.print("\n");
         }
@@ -75,19 +74,19 @@ public class Adventure {
         try { // make sure that the intended location is not outside of the 2D Array
             playField[oldX][oldY] = 0; // null out that number
             playField[x][y] = typeId; // set to new position
-        }
-        catch (ArrayIndexOutOfBoundsException e) {
+        } catch (ArrayIndexOutOfBoundsException e) {
             playField[oldX][oldY] = typeId;
-            
-        }
-        
-        
-            
 
+        }
+    }
+
+    public static void collisonCheck(int[][] playField) {
+        int x = findTypePosition(playField, 1).get(0);
+        int y = findTypePosition(playField, 1).get(0);
     }
 
     public static ArrayList<Integer> findTypePosition(int[][] playField, int typeId) {
-        ArrayList list = new ArrayList<Integer>();
+        ArrayList<Integer> list = new ArrayList<Integer>();
 
         int y = 0;
         boolean found = false;
