08d6c7428dc3d04741c67608c09f50d3f88161ff
Lukas Werner <program4cool@gmail.com>
2019-10-17 11:38:58 -0700

    Update FuncPlotter.java

--- UnitTwo/programs/FuncPlotter.java
+++ UnitTwo/programs/FuncPlotter.java
@@ -17,8 +17,30 @@ public class FuncPlotter extends JComponent {
         for (int i = 0; i <= 500; i+=50) {
             g.drawLine(0+i, 0, 0+i, 500);
         }
+        
         for (int i = 0; i <= 500; i+=50) {
-            g.drawLine(0, 0+i, 0, 500);
+            g.drawLine(0,0+i,500,0+i);
         }
+        int[] previousPoint = {0,0};
+        g.setColor(Color.BLUE); // quadratic
+        for (int i=0; i <= 500; i+=1) {
+            int x = i;
+            int y = (int)(Math.pow(i,2));
+            g.drawLine(previousPoint[0], previousPoint[1], x, y);
+            g.fillOval(x-2,y-2,4,4);
+            previousPoint[0] = x;
+            previousPoint[1] = y;
+        }
+        
+        g.setColor(Color.RED); // y = x
+        for (int x=0;x<=500; x+=1) {
+            g.drawOval(x,x,1,1);
+        }
+        g.setColor(Color.GREEN); //linear eqn
+        for (int i=0;i<=500;i+=1) {
+            int y = (3*i) +4;
+            g.fillOval(i-2, y-2, 4,4);
+        }
+
     }
 }
