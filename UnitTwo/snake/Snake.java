0743ada9c09ded63380b975523e5d995e983ae18
Lukas Werner <program4cool@gmail.com>
2019-10-18 10:32:28 -0700

    Add keyboard control to snake

--- UnitTwo/snake/Snake.java
+++ UnitTwo/snake/Snake.java
@@ -1,24 +1,26 @@
 import java.awt.*;
+import java.awt.event.*;
 import java.util.LinkedList;
+
 
 import javax.swing.*;
 
-public class Snake extends JComponent {
+public class Snake extends JComponent implements KeyListener {
     static LinkedList<int[]> snakeList = new LinkedList<int[]>(); //head at position 0
     static int dir = 0; // 0 UP 1 DOWN 2 LEFT 3 RIGHT
 
     public static void main(String[] args) {
-        JFrame window = new JFrame("My Drawing"); // create window with the title "My Drawing"
+        JFrame window = new JFrame("Snake"); // create window with the title "Snake"
         Snake snakes = new Snake();
 
+        snakes.addKeyListener(snakes);
         window.setSize(540, 540 + 23); // set up window frame to be a 540x540px window plus the 23 px offset
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // X button = close
 
-        window.add(snakes); // add shapes to the window
+        window.add(snakes); // add snakes to the window
 
         window.setVisible(true); // show window
         snakeList.addLast(new int[] {450,450});
-
         
         while (true) { // main function loop
             if (dir == 0) {snakeList.get(0)[1] -= 50;}
@@ -47,5 +49,22 @@ public class Snake extends JComponent {
             g.fillRect(bodyPart[0]+10, bodyPart[1]+10, 50, 50);
         }
 
+    }
+
+
+    private char c = ' ';
+    
+    public void addNotify() {
+      super.addNotify();
+      requestFocus();
+    }
+    public void keyPressed(KeyEvent e) { }
+    public void keyReleased(KeyEvent e) { }
+    public void keyTyped(KeyEvent e) { 
+        c = e.getKeyChar();
+        if (c == 'w') {dir = 0;}
+        if (c == 's') {dir = 1;}
+        if (c == 'a') {dir = 2;}
+        if (c == 'd') {dir = 3;}
     }
 }
