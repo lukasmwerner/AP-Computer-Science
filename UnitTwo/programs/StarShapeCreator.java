edf5a8a7003815a1b1c45c399997580721f0ce71
Lukas Werner <lwerner@lshigh.org>
2019-10-24 14:57:39 -0700

    Create StarShapeCreator.java

---
+++ UnitTwo/programs/StarShapeCreator.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class StarShapeCreator {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        System.out.print("What width of a square please? ");
+        String input = kb.nextLine();
+    }
+}
