134d39831f4d2c94788e54f306a421ad895ba2de
Lukas Werner <program4cool@gmail.com>
2019-10-29 11:15:26 -0700

    Dither

--- UnitTwo/programs/PictureEditor.java
+++ UnitTwo/programs/PictureEditor.java
@@ -1,4 +1,9 @@
 import java.awt.Color;
+import java.lang.Math;
+import java.io.PrintStream;
+import java.io.FileNotFoundException;
+
+import javax.xml.crypto.dsig.spec.TransformParameterSpec;
 
 public class PictureEditor {
     
@@ -11,11 +16,13 @@ public class PictureEditor {
         pic = new Picture(filename);
         System.out.println(pic.width() + "w x " + pic.height()+"h");
         
-        printPixelColors();
-        drawDarkerLine();
+        //printPixelColors();
+        //drawDarkerLine();
         //drawSquare(0,0,200);
         //invertPic();
-        greyScale(3);
+        greyScale(200);
+        dither();
+        //invertPic();
         pic.show();
             
     }
@@ -98,11 +105,39 @@ public class PictureEditor {
     public static void dither() {
         int width = pic.width();
         int height = pic.height();
+        
         for (int x = 0; x<width; x++) {
             for (int y = 0; y<height;y++) {
-                int below = pic.get(x,y+1).getRed();
-
-
+                int tempCalc = 0;
+                try {
+                    int current;
+                    int below = 255;
+                    int belowLeft = 255;
+                    int belowRight = 255;
+                    boolean belowYN = true;
+                    boolean belowLeftYN = true;
+                    boolean belowRightYN = true;
+                    int right = 255;
+                    boolean rightYN = true;
+                    current = pic.get(x,y).getRed();
+                    try {below = pic.get(x,y+1).getRed(); belowYN = true;} catch (ArrayIndexOutOfBoundsException e) {below = 0; belowYN = false;}
+                    try {belowLeft = pic.get(x-1,y+1).getRed(); belowLeftYN = true;}  catch (ArrayIndexOutOfBoundsException e) {belowLeft = 0; belowLeftYN = false;}
+                    try {belowRight = pic.get(x+1, y+1).getRed(); belowRightYN = true;} catch (ArrayIndexOutOfBoundsException e) {belowRight = 0; belowRightYN = false;}
+                    try {right = pic.get(x+1, y).getRed(); rightYN = true;} catch (ArrayIndexOutOfBoundsException e) {right = 0;rightYN = false;}
+                    int newVal = (int)Math.ceil(current /255);
+                    pic.set(x,y, new Color(newVal, newVal, newVal));
+                    int error = current - newVal;
+                    tempCalc = below + error * 5/16;
+                    if (belowYN) {pic.set(x,y+1, new Color(tempCalc, tempCalc, tempCalc));}
+                    tempCalc = belowLeft + error * 3/16;
+                    if (belowLeftYN) {pic.set(x-1,y+1, new Color(tempCalc, tempCalc, tempCalc));}
+                    tempCalc = belowRight + error * 1/16;
+                    if (belowRightYN) {pic.set(x+1, y+1, new Color(tempCalc, tempCalc, tempCalc));}
+                    tempCalc = right + error * 7/16;
+                    if (rightYN) {pic.set(x+1, y, new Color(tempCalc, tempCalc, tempCalc));}
+                } catch (IllegalArgumentException e) {
+                    System.out.println(tempCalc);
+                }
             }
         }
     }
