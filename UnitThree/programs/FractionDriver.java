835e0a64cb0abd53c89bab4b73b68bdf6ddebd6f
Lukas Werner <lwerner@lshigh.org>
2020-01-06 09:02:04 -0800

    changes

--- UnitThree/programs/FractionDriver.java
+++ UnitThree/programs/FractionDriver.java
@@ -1,7 +1,7 @@
 public class FractionDriver{ 
 public static void main(String[] args) {
-    Fraction f1 = new Fraction(500,1000);
-    Fraction f2 = new Fraction(1000,2000);
+    Fraction f1 = new Fraction(78,106);
+    Fraction f2 = new Fraction(1,2);
     Fraction f3 = f1.add(f2);
     System.out.println(f3);
     Fraction f4 = f1.mult(f2);
@@ -9,4 +9,8 @@ public static void main(String[] args) {
     Fraction f5 = new Fraction(24,90);
     f5.simp();
     System.out.println(f5);
+    System.out.println(blarg());
+}
+public static String blarg() {
+    return "Hello";
 }}
