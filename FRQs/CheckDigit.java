1cee1ba1d53163323d03c557ac6382ca82d278a9
Lukas Werner <41877874+bluetr00p@users.noreply.github.com>
2020-04-30 09:30:43 -0700

    Fix program 1...n issues

--- FRQs/CheckDigit.java
+++ FRQs/CheckDigit.java
@@ -3,26 +3,26 @@ public class CheckDigit {
   public static void main(String[] args) {
     String pass = "✅ Test Case Passed";
     String fail = "❌ Test Case Failed";
-    System.out.println(getCheck(283415) == 6 ? pass : fail);
-    System.out.println(getCheck(2183) == 2 ? pass : fail);
-    System.out.println(isValid(1592) == true ? pass : fail);
+    System.out.println(getCheck(283415) == 6 ? pass : fail + getCheck(283415));
+    System.out.println(getCheck(2183) == 2 ? pass : fail + getCheck(2183));
+    System.out.println(isValid(1592) == true ? pass : fail + isValid(1592));
   }
 
   public static int getCheck(int num) {
     int sum = 0;
     int mult = 7;
-    for (int i = 0; i < getNumberOfDigits(num); i++) {
+    for (int i = 1; i <= getNumberOfDigits(num); i++) {
       sum += getDigit(num, i) * mult;
       mult--;
     }
-    sum = getDigit(sum, getNumberOfDigits(sum) - 1);
+    sum = getDigit(sum, getNumberOfDigits(sum));
     return sum;
   }
 
   public static boolean isValid(int numWithCheckDigit) {
     String a = numWithCheckDigit + "";
     return (
-      getDigit(numWithCheckDigit, getNumberOfDigits(numWithCheckDigit) - 1) ==
+      getDigit(numWithCheckDigit, getNumberOfDigits(numWithCheckDigit)) ==
       getCheck(
         Integer.parseInt(
           a.substring(0, getNumberOfDigits(numWithCheckDigit) - 1)
@@ -37,6 +37,7 @@ public class CheckDigit {
 
   public static int getDigit(int num, int n) {
     String dString = num + "";
+    n = n -1;
     return Integer.parseInt(dString.charAt(n) + "");
   }
 
