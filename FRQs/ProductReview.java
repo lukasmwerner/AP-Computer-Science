767d357194ef91eb07365ba3dab3cebf13479f61
Lukas Werner <41877874+bluetr00p@users.noreply.github.com>
2020-06-02 21:26:33 -0700

    add Hailstone & ProductReview FRQs

---
+++ FRQs/ProductReview.java
@@ -0,0 +1,23 @@
+public class ProductReview {
+    private String name;
+    private String review;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public ProductReview(String name, String review) {
+        this.name = name;
+        this.review = review;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductReview [name=" + name + ", review=" + review + "]";
+    }
+    
+}
