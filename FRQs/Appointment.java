2165e05f641e6056fdebb924cea15a08c371a7a8
lwerner-lshigh <lwerner-lshigh@users.noreply.github.com>
2020-07-04 22:25:43 +0000

    Prettified Code!

--- FRQs/Appointment.java
+++ FRQs/Appointment.java
@@ -1,7 +1,11 @@
 public class Appointment {
-    private TimeInterval timeInterval;
-    public TimeInterval getTime() {return timeInterval;}
-    public boolean conflictsWith(Appointment other) {
-        return this.timeInterval.overlapsWith(other.getTime());
-    }
+  private TimeInterval timeInterval;
+
+  public TimeInterval getTime() {
+    return timeInterval;
+  }
+
+  public boolean conflictsWith(Appointment other) {
+    return this.timeInterval.overlapsWith(other.getTime());
+  }
 }
--- FRQs/TimeInterval.java
+++ FRQs/TimeInterval.java
@@ -1,5 +1,6 @@
 public class TimeInterval {
-    public boolean overlapsWith(TimeInterval interval) {
-        return true;
-    }
+
+  public boolean overlapsWith(TimeInterval interval) {
+    return true;
+  }
 }
