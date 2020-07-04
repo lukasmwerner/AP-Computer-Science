b0d4424adf1ba13a8c5f2f4cdbf2e818c8c14dd2
Lukas Werner <41877874+bluetr00p@users.noreply.github.com>
2020-07-04 15:25:06 -0700

    olde3r changes

---
+++ FRQs/Appointment.java
@@ -0,0 +1,7 @@
+public class Appointment {
+    private TimeInterval timeInterval;
+    public TimeInterval getTime() {return timeInterval;}
+    public boolean conflictsWith(Appointment other) {
+        return this.timeInterval.overlapsWith(other.getTime());
+    }
+}
---
+++ FRQs/DailySchedule.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+public class DailySchedule {
+    private ArrayList<Appointment> apptList; 
+    public DailySchedule() {
+        apptList = new ArrayList<Appointment>();
+    }
+    public void clearConflicts(Appointment appt) {
+        for (int i = 0; i < apptList.size(); i++) {
+            Appointment appointment = apptList.get(i);
+            if (appointment.conflictsWith(appt)) {
+                apptList.remove(i);
+                i--; //Avoid skipping errors (aka when we delete we skip too many)
+            }
+        }
+    }
+    public boolean addAppt(Appointment appt, boolean emergency) {
+        boolean conflicted = false;
+        for (int i = 0; i < apptList.size(); i++) {
+            Appointment appointment = apptList.get(i);
+            if (appointment.conflictsWith(appt)) {
+                conflicted = true;
+                break;
+            }
+        }
+        boolean didAdd = false;
+        if (emergency) {clearConflicts(appt);}
+        if (!conflicted || emergency) {
+            apptList.add(appt);
+            didAdd = true;
+        }
+        return didAdd;
+    }
+}
